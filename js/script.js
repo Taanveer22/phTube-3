d59c2d5763a747d3fe732f9efca08864a6fca8ab
Taanveer22 <taanveer469@gmail.com>
2025-08-04 15:24:34 +0600

    show categories and send data to display

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log('connected');
+console.log("connected");
+
+// loadCategoriesData
+const loadCategories = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => showCategoriesData(data.categories))
+    .catch((error) => console.log(error));
+};
+
+// showCategoriesData
+const showCategoriesData = (data) => {
+  console.log(data);
+};
+
+// final function invocation
+loadCategories();
